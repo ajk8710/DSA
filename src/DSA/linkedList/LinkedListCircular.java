3ab109031346c2210e0a30158fe11e43606019d4
Alex Kim <ajk8710@nyu.edu>
2023-09-19 20:04:29 -0400

    Circular Linked List Deletion

--- src/DSA/linkedList/LinkedListCircular.java
+++ src/DSA/linkedList/LinkedListCircular.java
@@ -4,6 +4,7 @@ public class LinkedListCircular {
     
     public static void main(String[] args) {
         CircularLinkedList list = new CircularLinkedList();
+        System.out.println("Insert Sorted");
         list.insertSorted(3);
         list.display();
         list.insertSorted(5);
@@ -14,9 +15,28 @@ public class LinkedListCircular {
         list.display();
         list.insertSorted(4);
         list.display();
-        
-        list = new CircularLinkedList();
+        System.out.println("Delete at position");
+        System.out.print("At 0: ");
+        list.deletePos(0);
         list.display();
+        System.out.print("At 3: ");
+        list.deletePos(3);
+        list.display();
+        System.out.print("At 1: ");
+        list.deletePos(1);
+        list.display();
+        System.out.print("At 1: ");
+        list.deletePos(1);
+        list.display();
+        System.out.print("At 0: ");
+        list.deletePos(0);
+        list.display();
+        System.out.print("At 0: ");
+        list.deletePos(0);
+        list.display();
+        
+        System.out.println();
+        System.out.println("Insert Sorted");
         list.insertSorted(3);
         list.display();
         list.insertSorted(2);
@@ -28,17 +48,56 @@ public class LinkedListCircular {
         list.insertSorted(1);
         list.display();
         
-        list = new CircularLinkedList();
+        System.out.println("Delete the value");
+        System.out.print("Val 2: ");
+        list.deleteVal(2);
         list.display();
+        System.out.print("Val 1: ");
+        list.deleteVal(1);
+        list.display();
+        System.out.print("Val 4: ");
+        list.deleteVal(4);
+        list.display();
+        System.out.print("Val 5: ");
+        list.deleteVal(5);
+        list.display();
+        System.out.print("Val 2: ");
+        list.deleteVal(2);
+        list.display();
+        System.out.print("Val 3: ");
+        list.deleteVal(3);
+        list.display();
+        System.out.print("Val 3: ");
+        list.deleteVal(3);
+        list.display();
+        
+        System.out.println("Insert At Position");
+        System.out.print("At 0, Val 1: ");
         list.insert(0, 1);  // insert(pos, val)
         list.display();
-        list.insert(0, -1);
+        System.out.print("At 0, Val 0: ");
+        list.insert(0, 0);
         list.display();
+        System.out.print("At 1, Val 3: ");
         list.insert(1, 3);
         list.display();
+        System.out.print("At 1, Val 2: ");
         list.insert(1, 2);
         list.display();
+        System.out.print("At 4, Val 5: ");
         list.insert(4, 5);
+        list.display();
+        System.out.println("Delete the value");
+        System.out.print("Val 2: ");
+        list.deleteVal(2);
+        list.display();
+        System.out.println("Delete at position");
+        System.out.print("At 0: ");
+        list.deletePos(0);
+        list.display();
+        System.out.println("Delete at position");
+        System.out.print("At 1: ");
+        list.deletePos(0);
         list.display();
     }
     
@@ -180,6 +239,84 @@ class CircularLinkedList {
         
         prev.next = newNode;  // insert between nodes
         newNode.next = curr;
+    }
+    
+    public void deleteVal(int val) {
+        if (head == null) {  // if list was empty
+            return;
+        }
+        
+        if (head == tail) {  // if list had only one node
+            if (head.val == val) {  // this code removes edge cases when deleting the first node
+                head = null;
+                tail = null;
+            }
+            return;
+        }
+        
+        Node2 prev = null;
+        Node2 curr = head;
+        
+        // at this point list must had at least two nodes
+        //   1 2 3
+        // p c
+        //   p c
+        while (curr != tail && curr.val != val) {  // find node with val
+            prev = curr;
+            curr = curr.next;
+        }
+        
+        if (prev == null) {    // deleting the first node
+            head = curr.next;  // curr is not tail as list must had at least two nodes
+            return;            // no need to update tail. no need to check if loop ended due to curr == tail or curr.val == val.
+        }
+        
+        if (curr.val != val) {  // value not found
+            return;
+        }
+        
+        prev.next = curr.next;
+        if (curr == tail) {  // update tail if deleted tail
+            tail = prev;
+        }
+    }
+    
+    public void deletePos(int pos) {
+        if (head == null) {  // if list was empty
+            return;
+        }
+        
+        if (head == tail) {  // if list had only one node
+            head = null;     // empty the list whatever pos is
+            tail = null;
+            return;
+        }
+        
+        Node2 prev = null;
+        Node2 curr = head;
+        
+        // at this point list must had at least two nodes
+        //   1 2 3
+        //     p c
+        for (int i = 0; i < pos; i++) {  // find position
+            prev = curr;
+            curr = curr.next;
+            
+            if (curr == tail) {  // if pos >= tail, delete last node
+                break;
+            }
+        }
+        
+        if (prev == null) {    // deleting the first node
+            head = curr.next;  // there is curr.next because list had at least two nodes
+            tail.next = head;  // tail points to new head
+            return;
+        }
+        
+        prev.next = curr.next;
+        if (curr == tail) {  // update tail if deleted tail
+            tail = prev;
+        }
     }
     
     public void display() {
---
+++ src/DSA/linkedList/problems/LinkedListSwapPairwise.java
@@ -0,0 +1,17 @@
+package DSA.linkedList.problems;
+
+// Given a singly linked list, write a function to swap elements pairwise.
+// Input : 1->2->3->4->5->6->NULL
+// Output : 2->1->4->3->6->5->NULL
+// Input : 1->2->3->4->5->NULL
+// Output : 2->1->4->3->5->NULL
+// Input : 1->NULL
+// Output : 1->NULL
+public class LinkedListSwapPairwise {
+
+    public static void main(String[] args) {
+        // TODO Auto-generated method stub
+
+    }
+
+}
